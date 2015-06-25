889ab7618b79faada1df1c50a150db0a3e1bd366
David H. Bronke <whitelynx@gmail.com>
2015-06-25 21:51:58 +0000

    Support overriding config options on the command line.
    Also, allow specifying the `DEBUG` option as an environment variable.

--- config.js
+++ config.js
@@ -4,9 +4,36 @@
 // @module config.js
 // ---------------------------------------------------------------------------------------------------------------------
 
+var _ = require('lodash');
+
+// ---------------------------------------------------------------------------------------------------------------------
+
 module.exports = {
-    DEBUG: true,
+    DEBUG: parseBool(process.env.DEBUG, true),
     port: 8008
 }; // end exports
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+var argv = require('minimist')(process.argv.slice(2), {boolean: true});
+_.merge(module.exports, _.omit(argv, '_'));
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+function parseBool(boolString, defaultValue)
+{
+    switch(boolString || ''.toLowerCase())
+    {
+        case '0':
+        case 'false':
+        case 'off':
+        case 'no':
+            return false;
+        case '':
+            return defaultValue;
+        default:
+            return true;
+    } // end switch
+} // end parseBool
 
 // ---------------------------------------------------------------------------------------------------------------------
