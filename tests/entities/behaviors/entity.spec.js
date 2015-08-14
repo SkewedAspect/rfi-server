87fc7c5e05a87439d2fdf496d171f22e83bf91c3
Christopher S. Case <chris.case@g33xnexus.com>
2015-08-13 19:39:34 -0500

    finished base entity unit tests.

--- tests/entities/behaviors/entity.spec.js
+++ tests/entities/behaviors/entity.spec.js
@@ -41,6 +41,86 @@ describe('BaseEntity', function()
         entity = new BaseEntity(entityDef);
     });
 
+    describe('Message Registration', function()
+    {
+        it('can register for events', function(done)
+        {
+            entity.registerEventHandler('test', function(args)
+            {
+                assert.equal(args.foo, 'bar');
+                done();
+            });
+
+            entity.onEvent('test', { foo: 'bar' });
+        });
+
+        it('can register for requests', function(done)
+        {
+            entity.registerRequestHandler('test', function(args)
+            {
+                assert.equal(args.foo, 'bar');
+                return Promise.resolve('worked!');
+            });
+
+            entity.onRequest('test', { foo: 'bar' }, function(resp)
+            {
+                assert.equal(resp, 'worked!');
+                done();
+            });
+        });
+
+        it('new registrations override previous registrations', function(done)
+        {
+            entity.registerEventHandler('test', function()
+            {
+                done(new Error('Did not override event handler!'));
+            });
+
+            entity.registerEventHandler('test', function()
+            {
+                done();
+            });
+
+            entity.onEvent('test');
+        });
+
+        it('can remove event registrations', function(done)
+        {
+            entity.registerEventHandler('test', function()
+            {
+                done(new Error('Did not remove event handler!'));
+            });
+
+            entity.removeEventHandler('test');
+
+            entity.onEvent('test');
+
+            // Wait, then call done.
+            setTimeout(function()
+            {
+                done();
+            }, 20);
+        });
+
+        it('can remove event registrations', function(done)
+        {
+            entity.registerRequestHandler('test', function()
+            {
+                done(new Error('Did not remove request handler!'));
+            });
+
+            entity.removeRequestHandler('test');
+
+            entity.onRequest('test', function(){});
+
+            // Wait, then call done.
+            setTimeout(function()
+            {
+                done();
+            }, 20);
+        });
+    });
+
     describe('.toJSON()', function()
     {
         it('returns an object with the properties `id` and `behavior`', function()
